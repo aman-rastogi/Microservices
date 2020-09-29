ec98e26d101620938f1718c3c5e9e9a34f800923
aman-rastogi <rastogiaman690@gmail.com>
2020-09-28 21:20:37 -0400

    Updated ports for all the services

--- movie-info-service/movie-info-service/src/main/java/com/mylearnings/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/movie-info-service/src/main/java/com/mylearnings/movieinfoservice/resources/MovieResource.java
@@ -1,2 +1,17 @@
-package com.mylearnings.movieinfoservice.resources;public class MovieResource {
+package com.mylearnings.movieinfoservice.resources;
+
+import com.mylearnings.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+        return new Movie(movieId,"Test Name");
+
+    }
 }
